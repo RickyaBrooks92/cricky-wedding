018cadd23b5a1aa82be3756f73c410c01f63d9ca
RickyTicky <Rickyabrooks92@gmail.com>
2023-02-15 19:48:55 -0500

    more github pages debugging

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,5 @@
-import React, { useEffect, useRef } from "react";
-
 const App = () => {
   // I want this component to render a video feed of the users phone camera
-
-  const videoRef = useRef<HTMLVideoElement>(null);
-
-  useEffect(() => {
-    if (videoRef.current) {
-      navigator.mediaDevices
-        .getUserMedia({ video: true })
-        .then((stream) => {
-          videoRef.current.srcObject = stream;
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    }
-  }, []);
 
   return <div>Hello World</div>;
 };
